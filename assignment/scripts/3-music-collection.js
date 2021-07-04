61ee81c2a61dfc46f839e47fb49b3c85cbf62345
Garrett Harty <gharty@live.com>
2021-07-04 17:22:03 -0500

    Completed and tested addToCollection

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,1 +1,42 @@
 console.log('***** Music Collection *****')
+
+// create an empty array named collection
+let collection = [];
+
+// create a function that takes in a title, artist, and year published and creates
+// a new object with those parameters
+function addToCollection(title, artist, yearPublished) {
+  const newObject = {
+    albumTitle: title,
+    albumArtist: artist,
+    albumYear: yearPublished
+  }
+  // add the created object to the end of the collection array
+  collection.push(newObject);
+  // return the new object
+  return newObject;
+}
+
+//test Collection
+console.log(`Collection is currently: ${collection}`);
+console.log('Adding 6 albums to collection',
+  addToCollection('Yellow Submarine', 'The Beatles', 1969),
+  addToCollection('The Black Parade', 'My Chemical Romance', 2006),
+  addToCollection('Danger Days', 'My Chemical Romance', 2010),
+  addToCollection('Rubber Soul', 'The Beatles', 1965),
+  addToCollection('Let it Bleed', 'The Rolling Stones', 1969),
+  addToCollection('Led Zeppelin', 'Led Zeppelin', 1969),
+);
+console.log(`Collection is now ${collection}`);
+
+// Create a function called showCollection to take in an array and log each item in the array to the Console
+function showCollection(array) {
+  console.log(`There are ${array.length} items in this collection.`);
+  for (item in array) {
+    console.log(`${item.albumTitle} by ${item.albumArtist}, published in ${item.albumYear}`);
+  }
+}
+// testing showCollection
+console.log(`Collection is now ${collection}`);
+console.log(collection[0].albumTitle);
+console.log('Testing showCollection(collection)', showCollection(collection));
